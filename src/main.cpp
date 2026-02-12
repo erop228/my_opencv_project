78b6bfc3f4b5dc8331f4c2e56d0a0a10e22b96b5
erop228 <soloveclijegor@gmail.com>
2026-02-13 00:44:12 +0300

    файлы первой лабы

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main() {
+    std::cout << "OpenCV version: " << CV_VERSION << std::endl;
+    
+    // Простая проверка - создать черное изображение
+    cv::Mat image(100, 100, CV_8UC3, cv::Scalar(0, 0, 255));
+    std::cout << "Image created: " << image.rows << "x" << image.cols << std::endl;
+    
+    return 0;
+}
