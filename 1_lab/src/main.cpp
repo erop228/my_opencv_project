43be15244106a103f4282da202bf8d4c5f57fe99
erop228 <soloveckijegor@gmail.com>
2026-02-15 02:33:57 +0300

    modified:   1_lab/.vscode/c_cpp_properties.json 	modified:   1_lab/.vscode/launch.json 	modified:   1_lab/.vscode/tasks.json 	modified:   1_lab/CMakeLists.txt 	new file:   1_lab/Gid.md 	modified:   1_lab/src/main.cpp

--- 1_lab/src/main.cpp
+++ 1_lab/src/main.cpp
@@ -3,10 +3,10 @@
 
 int main() {
     std::cout << "OpenCV version: " << CV_VERSION << std::endl;
-    
+
     // Простая проверка - создать черное изображение
     cv::Mat image(100, 100, CV_8UC3, cv::Scalar(0, 0, 255));
     std::cout << "Image created: " << image.rows << "x" << image.cols << std::endl;
-    
+
     return 0;
 }
